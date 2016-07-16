2c2cdea1817dcf4fdd9441a1cac217d83edc3b83
hanai <ihanai1991@gmail.com>
2016-07-16 15:30:57 +0800

    UPDATE

--- src/render.js
+++ src/render.js
@@ -16,6 +16,20 @@ function project(M) {
   return new Vertex2D(r * M.x, r * M.z);
 }
 
+/*
+function project(M, theta, C, E) {
+  const {sin, cos} = Math;
+  const dx = cos(theta.y) * (sin(theta.z) * (M.y - C.y) + cos(theta.z) * (M.x - C.x)) - sin(theta.y) * (M.z - C.z);
+  const dy = sin(theta.x) * (cos(theta.y) * (M.z - C.z) + sin(theta.y) * (sin(theta.z) * (M.y - C.y) + cos(theta.x) * (M.x - C.x))) + cos(theta.x) * (cos(theta.x) * (M.y - C.y) - sin(theta.z) * (M.x - C.x));
+  const dz = cos(theta.x) * (cos(theta.y) * (M.z - C.z) + sin(theta.y) * (sin(theta.z) * (M.y - C.y) + cos(theta.x) * (M.x - C.x))) - sin(theta.x) * (cos(theta.x) * (M.y - C.y) - sin(theta.z) * (M.x - C.x));
+
+  const tX = (dx - E.x)*(E.z / dz);
+  const tY = (dy - E.y)*(E.z / dz);
+
+  return new Vertex2D(tX, tY);
+}
+*/
+
 export function render(objects, ctx, dx, dy) {
   // Clear the previous frame
   ctx.clearRect(0, 0, 2 * dx, 2 * dy);
@@ -24,15 +38,15 @@ export function render(objects, ctx, dx, dy) {
   objects.forEach(object => {
     // for each face
     object.faces.forEach(face => {
-      // draw the first vertex
-      var P = project(face[0]);
-      ctx.beginPath();
-      ctx.moveTo(P.x + dx, -P.y + dy);
-
-      // draw the other vertices
       face.forEach((vertex, vertexIdx) => {
-        if (vertexIdx !== 0) {
-          P = project(vertex);
+        var P = project(vertex);
+
+        if (vertexIdx === 0) {
+          // draw the first vertex
+          ctx.beginPath();
+          ctx.moveTo(P.x + dx, -P.y + dy);
+        } else {
+          // draw the other vertices
           ctx.lineTo(P.x + dx, -P.y + dy);
         }
       });
